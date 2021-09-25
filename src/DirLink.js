a814f8d524181264efe8ea885160b69d52d7e1f9
Lars Wallenborn <lars@wallenborn.net>
2021-09-25 13:51:12 +0200

    correctly handle mouse clicks

--- src/DirLink.js
+++ src/DirLink.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Link from "./Link";
 
 export default function DirLink(props) {
-    return <Link onMouseUp={(event) => {
+    return <Link onMouseDown={(event) => {
+        if (event.button === 1) {
+            event.preventDefault();
+        }
+    }} onMouseUp={(event) => {
         switch (event.button) {
             case 0: // left
                 props.loadFunc(props.dir);
                 break;
             case 1: // middle
                 window.open(window.location.href.match(/(^[^#]*)/)[0] + '#' + props.dir, '_blank');
+                event.preventDefault();
                 break;
             default:
                 break
